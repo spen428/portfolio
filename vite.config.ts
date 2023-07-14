410a9e49360ebd6458788475cde4ee6f5156ca76
Samuel P <77985620+spen428@users.noreply.github.com>
2023-07-14 20:03:31 +0100

    Make vite fail if port is in use (#1)
    Co-authored-by: spen428 <spen428@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
