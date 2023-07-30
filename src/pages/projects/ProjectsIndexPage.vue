2df1439f9a298b004b64ab1d68836f0eba9d6dfd
spen428 <spen428@users.noreply.github.com>
2023-07-30 22:40:50 +0100

    Refactor

--- src/pages/projects/ProjectsIndexPage.vue
+++ src/pages/projects/ProjectsIndexPage.vue
@@ -1,38 +1,12 @@
 <template>
-  <div class="relative mx-32">
-    <ProjectDiamond
-      v-for="(project, index) in projects"
-      :key="project.id"
-      :project="project"
-      :size="placements[index].size"
-      class="absolute"
-      :style="{
-        top: placements[index].top,
-        left: placements[index].left,
-      }"
-    />
-  </div>
+  <DiamondGrid :projects="projects" />
 </template>
 
 <style scoped></style>
 
 <script setup lang="ts">
+import DiamondGrid from "@/pages/projects/components/DiamondGrid.vue";
 import DataService from "@/services/DataService";
-import ProjectDiamond from "@/pages/projects/components/ProjectDiamond.vue";
-import { computed } from "vue";
 
-const placements = [
-  { size: "32rem", left: "0", top: "8rem" },
-  { size: "32rem", left: "32rem", top: "8rem" },
-  { size: "16rem", left: "48rem", top: "0" },
-  { size: "16rem", left: "24rem", top: "8rem" },
-  { size: "16rem", left: "32rem", top: "32rem" },
-  { size: "16rem", left: "48rem", top: "32rem" },
-  { size: "32rem", left: "56rem", top: "16rem" },
-  { size: "32rem", left: "72rem", top: "0" },
-];
-
-const projects = computed(() =>
-  DataService.getProjects().slice(0, placements.length)
-);
+const projects = DataService.getProjects();
 </script>
--- src/pages/projects/components/DiamondGrid.vue
+++ src/pages/projects/components/DiamondGrid.vue
@@ -5,12 +5,15 @@
     :style="{ height: containerHeightPx + 'px' }"
   >
     <ProjectDiamond
-      v-for="diamond in diamonds"
-      :key="diamond.id"
-      :size="diamondSizeRem + 'rem'"
-      :project="diamond.project"
-      class="!absolute"
-      :style="{ inset: getTranslation(diamond.position) }"
+      v-for="(project, index) in projects"
+      :key="project.id"
+      :project="project"
+      :size="placements[index].size"
+      class="absolute"
+      :style="{
+        top: placements[index].top,
+        left: placements[index].left,
+      }"
     />
   </div>
 </template>
@@ -21,53 +24,25 @@ import ProjectDiamond from "@/pages/projects/components/ProjectDiamond.vue";
 import type { Ref } from "vue";
 import { computed, onBeforeUnmount, onMounted, ref } from "vue";
 
-const props = defineProps<{
-  diamondSizeRem: number;
-  spacingRem: number;
-  projects: Project[];
-}>();
+defineProps<{ projects: Project[] }>();
 
-type Diamond = {
-  project: Project | null;
-  index: number;
-  id: string;
-  position: Position;
-};
+const leftOffsetRem = 6;
+const topOffsetRem = 1;
 
-const diamonds = computed(() => {
-  const arr: Diamond[] = props.projects.map((project, index) => ({
-    id: project.id + "",
-    project,
-    position: getRowCol(index),
-    index,
-  }));
-
-  let counter = 0;
-
-  function placeholder(row: number, col: number) {
-    return {
-      id: "placeholder" + counter++,
-      project: null,
-      position: { row, col },
-      index: row * col,
-    };
-  }
-
-  for (let i = arr.length; i < colsPerRow.value * totalRows.value; i++) {
-    const { row, col } = getRowCol(i);
-    arr.push(placeholder(row, col));
-  }
-  for (let i = 0; i < colsPerRow.value + 1; i++) {
-    arr.push(placeholder(totalRows.value, i));
-    arr.push(placeholder(totalRows.value + 1, i));
-  }
-  for (let i = 0; i < totalRows.value + 1; i++) {
-    arr.push(placeholder(i, -1));
-    arr.push(placeholder(i, colsPerRow.value));
-  }
-
-  return arr.sort((a, b) => b.index - a.index);
-});
+const placements = [
+  { size: 32, left: 0, top: 8 },
+  { size: 32, left: 32, top: 8 },
+  { size: 16, left: 48, top: 0 },
+  { size: 16, left: 24, top: 8 },
+  { size: 16, left: 32, top: 32 },
+  { size: 16, left: 48, top: 32 },
+  { size: 32, left: 56, top: 16 },
+  { size: 32, left: 72, top: 0 },
+].map((placement) => ({
+  size: placement.size + "rem",
+  left: leftOffsetRem + placement.left + "rem",
+  top: topOffsetRem + placement.top + "rem",
+}));
 
 const container: Ref<HTMLDivElement | undefined> = ref();
 const containerWidthPx = ref(0);
@@ -76,81 +51,6 @@ const isDesktopSize = computed(() => {
   const breakpointWidthPx = 960; // TODO: Extract from tailwind config
   return containerWidthPx.value >= breakpointWidthPx;
 });
-const diamondSizePx = computed(() => remToPx(props.diamondSizeRem));
-const spacingPx = computed(() => remToPx(props.spacingRem));
-const xOffsetPx = computed(() => diamondSizePx.value + spacingPx.value);
-const yOffsetPx = computed(() => diamondSizePx.value * 0.5 + spacingPx.value);
-const totalRows = computed(() => {
-  return Math.floor(
-    (containerHeightPx.value - yOffsetPx.value) / yOffsetPx.value
-  );
-});
-const colsPerRow = computed(() => {
-  if (!isDesktopSize.value) {
-    return Math.ceil(props.projects.length / totalRows.value);
-  }
-
-  return Math.floor(containerWidthPx.value / xOffsetPx.value);
-});
-const colsPerTwoRows = computed(() => {
-  if (!isDesktopSize.value) {
-    return 2 * colsPerRow.value;
-  }
-
-  return (
-    2 * colsPerRow.value -
-    (containerWidthPx.value % xOffsetPx.value >= xOffsetPx.value / 2 ? 0 : 1)
-  );
-});
-const leftMarginPx = computed(() => {
-  if (!isDesktopSize.value) {
-    return 16;
-  }
-
-  let widthPx = colsPerRow.value * xOffsetPx.value;
-  if (colsPerTwoRows.value % 2 === 0) {
-    widthPx += diamondSizePx.value / 2;
-  }
-  return (containerWidthPx.value - widthPx) / 2;
-});
-const topMarginPx = computed(() => {
-  const heightPx = totalRows.value * yOffsetPx.value + yOffsetPx.value;
-  const marginPx = (containerHeightPx.value - heightPx) / 2;
-
-  if (isDesktopSize.value) {
-    return marginPx * 0.5 + xOffsetPx.value / 2;
-  }
-  return marginPx;
-});
-
-type Position = { row: number; col: number };
-
-function getRowCol(index: number): Position {
-  let row = 2 * Math.floor(index / colsPerTwoRows.value);
-  let col = index % colsPerTwoRows.value;
-
-  if (col >= colsPerRow.value) {
-    row += 1;
-    col -= colsPerRow.value;
-  }
-
-  return { row, col };
-}
-
-function getTranslation(position: Position): string {
-  if (containerWidthPx.value === 0) {
-    return "0";
-  }
-
-  const { row, col } = position;
-
-  const oddRowXOffset = (row % 2) * xOffsetPx.value * 0.5;
-
-  const x = col * xOffsetPx.value + oddRowXOffset + leftMarginPx.value;
-  const y = row * yOffsetPx.value + topMarginPx.value;
-
-  return `${y}px ${x}px`;
-}
 
 const baseFontSize = parseFloat(
   getComputedStyle(document.documentElement).fontSize
