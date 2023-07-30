d084d6f0ac83f4d7b8eed60a527d6986e8cafb06
spen428 <spen428@users.noreply.github.com>
2023-07-30 22:31:44 +0100

    Refactor

--- src/pages/projects/ProjectsIndexPage.vue
+++ src/pages/projects/ProjectsIndexPage.vue
@@ -1,44 +1,15 @@
 <template>
   <div class="relative mx-32">
     <ProjectDiamond
-      :project="projects[0]"
-      size="32rem"
-      class="absolute left-0 top-32"
-    />
-    <ProjectDiamond
-      :project="projects[1]"
-      size="32rem"
-      class="absolute left-[32rem] top-32"
-    />
-    <ProjectDiamond
-      :project="projects[2]"
-      size="16rem"
-      class="absolute left-[48rem] top-0"
-    />
-    <ProjectDiamond
-      :project="projects[3]"
-      size="16rem"
-      class="absolute left-[24rem] top-[8rem]"
-    />
-    <ProjectDiamond
-      :project="projects[4]"
-      size="16rem"
-      class="absolute left-[32rem] top-[32rem]"
-    />
-    <ProjectDiamond
-      :project="projects[5]"
-      size="16rem"
-      class="absolute left-[48rem] top-[32rem]"
-    />
-    <ProjectDiamond
-      :project="projects[6]"
-      size="32rem"
-      class="absolute left-[56rem] top-[16rem]"
-    />
-    <ProjectDiamond
-      :project="projects[7]"
-      size="32rem"
-      class="absolute left-[72rem] top-0"
+      v-for="(project, index) in projects"
+      :key="project.id"
+      :project="project"
+      :size="placements[index].size"
+      class="absolute"
+      :style="{
+        top: placements[index].top,
+        left: placements[index].left,
+      }"
     />
   </div>
 </template>
@@ -49,68 +20,19 @@
 import DataService from "@/services/DataService";
 import ProjectDiamond from "@/pages/projects/components/ProjectDiamond.vue";
 import { computed } from "vue";
-import type { Project } from "@/services/IDataService";
 
-type VertexMeetingPosition =
-  | "default"
-  | "topToLeft"
-  | "leftToTop"
-  | "leftToRight";
-
-interface PositionedProjectDiamond {
-  project: Project;
-  size: string;
-  top: string;
-  left: string;
-}
-
-const defs: { size: number; position: VertexMeetingPosition }[] = [
-  { size: 32, position: "default" },
-  { size: 16, position: "leftToTop" },
-  { size: 32, position: "leftToRight" },
-  { size: 16, position: "leftToTop" },
+const placements = [
+  { size: "32rem", left: "0", top: "8rem" },
+  { size: "32rem", left: "32rem", top: "8rem" },
+  { size: "16rem", left: "48rem", top: "0" },
+  { size: "16rem", left: "24rem", top: "8rem" },
+  { size: "16rem", left: "32rem", top: "32rem" },
+  { size: "16rem", left: "48rem", top: "32rem" },
+  { size: "32rem", left: "56rem", top: "16rem" },
+  { size: "32rem", left: "72rem", top: "0" },
 ];
 
-const projects = computed(() => DataService.getProjects());
-const positionedProjectDiamonds = computed(() => map(projects.value));
-
-function calculateOffset(
-  previous: PositionedProjectDiamond,
-  current: { size: number; position: VertexMeetingPosition }
-): { top: string; left: string } {
-  switch (current.position) {
-    case "default":
-      break;
-    case "leftToRight":
-      return {
-        top: previous.top,
-        left: `calc(${previous.left}+${previous.size})`,
-      };
-    case "topToLeft":
-      break;
-    case "leftToTop":
-      break;
-  }
-  return {
-    top: "0",
-    left: "0",
-  };
-}
-
-function map(projects: Project[]): PositionedProjectDiamond[] {
-  const out: PositionedProjectDiamond[] = [];
-
-  for (let i = 0; i < projects.length; i++) {
-    const project = projects[i];
-    const previous = out[i - 1];
-    const def = defs[i];
-    out.push({
-      project,
-      size: def.size + "rem",
-      ...calculateOffset(previous, def),
-    });
-  }
-
-  return out;
-}
+const projects = computed(() =>
+  DataService.getProjects().slice(0, placements.length)
+);
 </script>
