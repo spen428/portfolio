36ce1c3b9be3c562ef80c9f6af77c1fd41e4c1e8
spen482 <spen482@users.noreply.github.com>
2023-12-04 02:04:12 +0000

    Auto highlight diamonds on horizontal scroll for small screens because they cant hover

--- src/pages/projects/components/DiamondGrid.vue
+++ src/pages/projects/components/DiamondGrid.vue
@@ -1,19 +1,22 @@
 <template>
   <div
-    class="relative w-full overflow-y-hidden overflow-x-scroll lg:overflow-x-hidden"
+    class="relative w-full overflow-scroll lg:overflow-x-hidden"
     ref="container"
     :style="{ height: containerHeightPx + 'px' }"
+    @scroll="updateScrollPosition()"
   >
     <ProjectDiamond
       v-for="(project, index) in projects"
+      :id="`project-diamond-` + index"
       :key="project.id"
       :project="project"
-      :size="placements[index].size"
+      :size="placements[index].sizeRem + `rem`"
       class="absolute"
       :style="{
-        top: placements[index].top,
-        left: placements[index].left,
+        top: placements[index].topRem + `rem`,
+        left: placements[index].leftRem + `rem`,
       }"
+      :active="isActive(`project-diamond-` + index)"
     />
   </div>
 </template>
@@ -29,7 +32,8 @@ defineProps<{ projects: Project[] }>();
 const leftOffsetRem = 6;
 const topOffsetRem = 1;
 
-const placements = [
+type Placement = { sizeRem: number; topRem: number; leftRem: number };
+const placements: Placement[] = [
   { size: 32, left: 0, top: 8 },
   { size: 32, left: 32, top: 8 },
   { size: 16, left: 48, top: 0 },
@@ -39,9 +43,9 @@ const placements = [
   { size: 32, left: 56, top: 16 },
   { size: 32, left: 72, top: 0 },
 ].map((placement) => ({
-  size: placement.size + "rem",
-  left: leftOffsetRem + placement.left + "rem",
-  top: topOffsetRem + placement.top + "rem",
+  sizeRem: placement.size,
+  leftRem: leftOffsetRem + placement.left,
+  topRem: topOffsetRem + placement.top,
 }));
 
 const container: Ref<HTMLDivElement | undefined> = ref();
@@ -51,6 +55,8 @@ const isDesktopSize = computed(() => {
   const breakpointWidthPx = 960; // TODO: Extract from tailwind config
   return containerWidthPx.value >= breakpointWidthPx;
 });
+
+const scrollLeft = ref(0);
 
 const baseFontSize = parseFloat(
   getComputedStyle(document.documentElement).fontSize
@@ -65,11 +71,36 @@ function updateContainerSize() {
   containerHeightPx.value = container.value?.parentElement?.offsetHeight ?? 0;
 }
 
+function updateScrollPosition() {
+  if (!container.value) return;
+  scrollLeft.value = container.value.scrollLeft;
+}
+
+const activeBounds = computed(() => [
+  containerWidthPx.value * 0.35,
+  containerWidthPx.value * 0.85,
+]);
+
+function isActive(elementId: string): boolean {
+  if (isDesktopSize.value) return false;
+
+  const element = document.getElementById(elementId);
+  if (!element) return false;
+
+  const elementCenter = element.offsetLeft + element.offsetWidth / 2;
+  const actualCenter = elementCenter - scrollLeft.value;
+
+  return (
+    actualCenter >= activeBounds.value[0] &&
+    actualCenter <= activeBounds.value[1]
+  );
+}
+
 onMounted(() => {
   window.addEventListener("resize", updateContainerSize);
   updateContainerSize();
 });
-onBeforeUnmount(() =>
-  window.removeEventListener("resize", updateContainerSize)
-);
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", updateContainerSize);
+});
 </script>
