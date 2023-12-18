dde792731d272b2672814c236044188bd2be080d
spen482 <spen482@users.noreply.github.com>
2023-12-18 01:54:47 +0000

    More skeletons

--- web/src/pages/home/HomePage.vue
+++ web/src/pages/home/HomePage.vue
@@ -2,7 +2,10 @@
   <div
     class="mx-auto mt-12 flex w-full max-w-6xl flex-col items-center justify-start 2xl:max-w-7xl"
   >
-    <Skeleton v-if="!personalInfo.fullName" class="mb-2 !h-8 !w-[30vw]" />
+    <Skeleton
+      v-if="!personalInfo.fullName"
+      class="mb-2 !h-9 !w-[30vw] !bg-cv-dark-purple"
+    />
     <h1
       v-else
       class="text-shadow mb-2 font-serif text-[min(1.5rem,5vw)] font-bold text-cv-dark-purple text-opacity-80"
@@ -13,7 +16,10 @@
       </ruby>
       <span v-else>&centerdot; {{ personalInfo.fullName }} &centerdot;</span>
     </h1>
+
+    <Skeleton v-if="!personalInfo.tagLine" class="mb-8 !h-10 !w-[45vw]" />
     <h2
+      v-else
       class="text-shadow mb-8 font-serif text-[min(1.85rem,5vw)] font-bold italic text-cv-white"
     >
       {{ personalInfo.tagLine }}
@@ -23,6 +29,7 @@
       :alt="$t('logo')"
       class="w-[45vw] animate-sideToSide select-none xs:h-64 xs:w-[unset]"
     />
+
     <div class="mb-12 flex flex-col items-center gap-6 pt-10 xs:hidden">
       <router-link to="/portfolio/cv" class="text-cv-white">
         <button class="rounded-lg border px-4 py-2">
