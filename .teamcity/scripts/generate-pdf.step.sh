8fc528a0a5295983dd479b0e6d533dc58f69fe9b
spen482 <spen482@users.noreply.github.com>
2023-12-18 22:09:02 +0000

    More scripts

--- .teamcity/scripts/generate-pdf.step.sh
+++ .teamcity/scripts/generate-pdf.step.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
+source "$(dirname "$0")/common.step.sh"
+
 export DEBUG=express:router
 export DATA_PATH=data
 export NODE_ENV=production
 export SERVER_PORT=15000
-pnpm run run:prod &
-pid=$!
 
-while true; do
-  echo "Waiting for API to start..."
-  curl -s "http://localhost:${SERVER_PORT}" > /dev/null
-  [ $? -ne 0 ] || break
-  sleep 1
-done
-while true; do
-  echo "Waiting for Vite to start..."
-  curl -s "http://localhost:5173" > /dev/null
-  [ $? -ne 0 ] || break
-  sleep 1
-done
-echo "Ready."
+start_services $SERVER_PORT 5173
 
 pnpm run --filter web pdf
 result=$?
 
-kill -INT $pid || true
-sleep 1
-
-while [ -n "$(ps | grep $pid)" ]; do
-  kill -KILL $pid || true
-  sleep 1
-done
+stop_services
 
 if [ $result -ne 0 ]; then
   exit $result
 fi
 
-DATE=$(date '+%Y-%m-%d')
-mv web/bin/CV.pdf "web/bin/[${DATE}] CV.pdf"
+mv web/bin/CV.pdf "web/bin/[$(date '+%Y-%m-%d')] CV.pdf"
