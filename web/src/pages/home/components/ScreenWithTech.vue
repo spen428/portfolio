d8828e309c25754dbf7c7d4d5535e7961e334fc9
spen482 <spen482@users.noreply.github.com>
2023-12-23 12:51:48 +0000

    almost there

--- web/src/pages/home/components/ScreenWithTech.vue
+++ web/src/pages/home/components/ScreenWithTech.vue
@@ -1,17 +1,9 @@
 <template>
   <div
-    class="flex h-72 w-full items-center justify-center overflow-hidden md:gap-4 lg:mx-8 lg:gap-16"
-    :class="{ 'flex-row-reverse': imagePosition === 'right' }"
+    class="flex w-full flex-col items-center justify-center gap-10 overflow-hidden md:h-72 md:flex-row md:gap-4 lg:mx-8 lg:gap-16"
+    :class="{ 'md:flex-row-reverse': imagePosition === 'right' }"
   >
-    <div
-      class="relative mx-4 h-full md:static"
-      :class="{
-        'right-3/5': imagePosition === 'left',
-        'xs:right-1/3': imagePosition === 'left',
-        'left-3/5': imagePosition === 'right',
-        'xs:left-1/3': imagePosition === 'right',
-      }"
-    >
+    <div class="mx-4 h-full md:static">
       <img
         :src="imageUrl"
         alt=""
@@ -20,25 +12,22 @@
     </div>
 
     <div
-      class="flex h-full shrink grow flex-col justify-between md:static"
+      class="flex h-full w-3/5 grow flex-wrap justify-between gap-4 md:static md:w-auto md:shrink md:flex-col md:flex-nowrap"
       :class="{
-        'left-[calc(55%+1rem)] xs:left-[calc(70%)]': imagePosition === 'left',
-        'right-[calc(55%+1rem)] xs:right-[calc(70%)]':
-          imagePosition === 'right',
+        'md:mr-8': imagePosition === 'left',
+        'md:ml-8': imagePosition === 'right',
       }"
     >
       <div
         v-for="(tech, index) in technologies"
         :key="tech.name"
         class="inline-flex items-center gap-4"
-        :class="{ 'self-end': index % 2 == 1 }"
+        :class="{ 'md:self-end': index % 2 == 1 }"
       >
         <img :src="tech.logoUrl" alt="" class="h-12 w-12" />
         <span class="font-bold" :style="{ color: '#fff' }" v-text="tech.name" />
       </div>
     </div>
-
-    <div id="spacer" class="shrink grow bg-pink-500">.</div>
   </div>
 </template>
 
