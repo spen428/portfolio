659331d6f63062c04dfb26b1ea6befffbe2b56ad
spen482 <spen482@users.noreply.github.com>
2023-12-24 22:09:53 +0000

    Fail if the ci report is missing

--- .teamcity/scripts/run-tests.step.sh
+++ .teamcity/scripts/run-tests.step.sh
@@ -4,11 +4,12 @@ source "$(dirname "$0")/common.step.sh"
 export DEBUG=express:router
 export SERVER_PORT=15000
 
-start_services $SERVER_PORT 5173
-
 pnpm run test
 result=$?
 
-stop_services
+if [ ! -r "web/visual_regressions/ci_report/xunit.xml" ]; then
+  echo "No test report could be found after running the tests."
+  exit 33
+fi
 
 exit $result
