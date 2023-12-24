07f0684fd181a9573cb5691e19b7d6094042ea1a
spen482 <spen482@users.noreply.github.com>
2023-12-24 00:21:51 +0000

    wut

--- web/src/pages/business-card/components/BusinessCardBack.vue
+++ web/src/pages/business-card/components/BusinessCardBack.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="relative border-2 border-cv-white bg-cv-white">
+  <div
+    class="relative flex h-full w-full items-center justify-evenly border-2 border-cv-white bg-cv-white"
+  >
     <svg
       width="100%"
       height="100%"
       viewBox="0 0 1 1"
       preserveAspectRatio="none"
-      class="absolute inset-0 z-0"
+      class="absolute inset-0"
     >
       <polygon points="0,0 0,0.1 1,0.05 1,0" class="fill-cv-dark-purple" />
       <polygon points="1,1 1,0.9 0,0.95 0,1" class="fill-cv-purple" />
     </svg>
 
-    <div class="flex h-full w-full items-center justify-center">
-      <img src="/logo.svg" alt="Logo" class="h-1/3" />
+    <div class="flex h-full w-full items-center px-2 py-8">
+      <img src="/logo.svg" alt="" class="mx-auto h-1/2" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { PersonalInfo } from "@/services/data.model";
+import type { CvData } from "@/services/data.model";
 
-defineProps<{ personalInfo: PersonalInfo }>();
+defineProps<{ cvData: CvData }>();
 </script>
