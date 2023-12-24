122ae1ad1366a6835781e2404e5b68e606a2b7e8
spen482 <spen482@users.noreply.github.com>
2023-12-24 19:44:29 +0000

    fixed

--- docker/scripts/api-command.sh
+++ docker/scripts/api-command.sh
@@ -2,6 +2,7 @@
 mkdir -p /app
 cp -r /src/* /app
 cd /app || exit
+rm -rf node_modules/
 
 npm install -g pnpm
 pnpm install
--- docker/scripts/web-command.sh
+++ docker/scripts/web-command.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -r /src/* /app
 cd /app || exit
+rm -rf node_modules/
 
 npm install -g pnpm
 pnpm install
