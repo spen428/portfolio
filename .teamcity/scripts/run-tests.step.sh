75172a6c582a0e0174e027329660afa157c3fc92
spen482 <spen482@users.noreply.github.com>
2023-12-25 23:41:35 +0000

    nfio

--- .teamcity/scripts/run-tests.step.sh
+++ .teamcity/scripts/run-tests.step.sh
@@ -4,19 +4,19 @@ function dc() {
 }
 
 cd "$(dirname "$0")/../../docker" || exit 4
-volume=$(< /proc/sys/kernel/random/uuid)
+export VR_VOLUME_NAME=$(< /proc/sys/kernel/random/uuid)
 
 dc build
-docker volume rm "$volume" || true
+docker volume rm "$VR_VOLUME_NAME" || true
 dc up --abort-on-container-exit vr || true
-dc run --rm --detach --name=post_vr --volume="$volume" --entrypoint "sleep infinity" vr
-docker cp post_vr:/src/visual_regressions/ci_report    ../web/visual_regressions
-docker cp post_vr:/src/visual_regressions/html_report  ../web/visual_regressions
-docker cp post_vr:/src/visual_regressions/bitmaps_test ../web/visual_regressions
-docker cp post_vr:/src/visual_regressions/pdf_test ../web/visual_regressions
-docker cp post_vr:/src/bin ../web
-docker volume rm "$volume" || true
+container=$(dc run --rm --detach --volume="$VR_VOLUME_NAME" --entrypoint "sleep infinity" vr)
+docker cp $container:/src/visual_regressions/ci_report    ../web/visual_regressions
+docker cp $container:/src/visual_regressions/html_report  ../web/visual_regressions
+docker cp $container:/src/visual_regressions/bitmaps_test ../web/visual_regressions
+docker cp $container:/src/visual_regressions/pdf_test ../web/visual_regressions
+docker cp $container:/src/bin ../web
 dc down
+docker volume rm "$VR_VOLUME_NAME" || true
 
 if [ ! -r "../web/visual_regressions/ci_report/xunit.xml" ]; then
   echo "No test report could be found after running the tests."
