d143d9fc11f409aac2944de78a308aa7f6a8f7cb
spen482 <spen482@users.noreply.github.com>
2023-12-25 15:07:08 +0000

    sjfidjsof

--- .teamcity/scripts/run-tests.step.sh
+++ .teamcity/scripts/run-tests.step.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-source "$(dirname "$0")/common.step.sh"
+function dc() {
+  docker-compose -f compose.test.yml "$@"
+}
 
-export DEBUG=express:router
-export SERVER_PORT=15000
+cd "$(dirname "$0")/../../docker" || exit 4
 
-cd docker || exit 4
-alias dc="docker-compose -f compose.test.yml"
 dc build && \
-  dc run --rm -detach --name=pre_vr --entrypoint "sleep infinity" vr && \
-  docker cp ../web/backstop.cjs       pre_vr:/src && \
-  docker cp ../web/scripts            pre_vr:/src && \
-  docker cp ../web/visual_regressions pre_vr:/src && \
-  dc down && \
   dc up --abort-on-container-exit vr && \
   dc run --rm -detach --name=pre_vr --entrypoint "sleep infinity" vr && \
-  docker cp pre_vr:/src/visual_regressions/ci_report    ../web/visual_regressions && \
-  docker cp pre_vr:/src/visual_regressions/html_report  ../web/visual_regressions && \
-  docker cp pre_vr:/src/visual_regressions/bitmaps_test ../web/visual_regressions && \
+  docker cp pre_vr:/app/visual_regressions/ci_report    ../web/visual_regressions && \
+  docker cp pre_vr:/app/visual_regressions/html_report  ../web/visual_regressions && \
+  docker cp pre_vr:/app/visual_regressions/bitmaps_test ../web/visual_regressions && \
   dc down
 result=$?
 
--- web/Backstop.Dockerfile
+++ web/Backstop.Dockerfile
@@ -1,12 +1,18 @@
 FROM backstopjs/backstopjs:6.3.2
 
 RUN apt-get update && \
-    apt-get install -y ghostscript bash
+    apt-get install -y ghostscript bash && \
+    npm install -g pnpm
 
-WORKDIR /src
-VOLUME /src
+WORKDIR /app
+
+COPY package.json pnpm-lock.yaml ./
+RUN pnpm install
+COPY backstop.cjs .
+COPY visual_regressions/ visual_regressions/
+COPY scripts/ scripts/
 
 ENV BASE_URL=http://web:5173
-ENTRYPOINT find . && node scripts/export-pdf.js --url="$BASE_URL" && \
-             bash scripts/generate-pngs-from-pdf.sh && \
-             backstop test --configPath=backstop.cjs
+ENTRYPOINT node scripts/export-pdf.js --url="$BASE_URL" && \
+    bash scripts/generate-pngs-from-pdf.sh && \
+    backstop test --configPath=backstop.cjs
