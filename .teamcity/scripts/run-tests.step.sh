d96a6c5d5e766a30c8fd731e3d324a553d9172c9
spen482 <spen482@users.noreply.github.com>
2023-12-25 14:09:50 +0000

    try copying the files to and from the container instead

--- .teamcity/scripts/run-tests.step.sh
+++ .teamcity/scripts/run-tests.step.sh
@@ -3,18 +3,24 @@ source "$(dirname "$0")/common.step.sh"
 
 export DEBUG=express:router
 export SERVER_PORT=15000
-composeFile="compose.test.yml"
 
-#pnpm run test
 cd docker || exit 4
-find ../web
-docker-compose -f $composeFile build && \
-  docker-compose -f $composeFile up --abort-on-container-exit vr
+alias dc="docker-compose -f compose.test.yml"
+dc build && \
+  dc run --rm -detach --name=pre_vr --entrypoint "sleep infinity" vr && \
+  docker cp ../web/backstop.cjs       pre_vr:/src && \
+  docker cp ../web/scripts            pre_vr:/src && \
+  docker cp ../web/visual_regressions pre_vr:/src && \
+  dc down && \
+  dc up --abort-on-container-exit vr && \
+  dc run --rm -detach --name=pre_vr --entrypoint "sleep infinity" vr && \
+  docker cp pre_vr:/src/visual_regressions/ci_report    ../web/visual_regressions && \
+  docker cp pre_vr:/src/visual_regressions/html_report  ../web/visual_regressions && \
+  docker cp pre_vr:/src/visual_regressions/bitmaps_test ../web/visual_regressions && \
+  dc down
 result=$?
 
-docker-compose -f $composeFile down
-
-if [ ! -r "web/visual_regressions/ci_report/xunit.xml" ]; then
+if [ ! -r "../web/visual_regressions/ci_report/xunit.xml" ]; then
   echo "No test report could be found after running the tests."
   exit 33
 fi
