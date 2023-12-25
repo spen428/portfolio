0b37f834c349548711415eba0dd9ac4ae17dcc17
spen482 <spen482@users.noreply.github.com>
2023-12-25 23:27:10 +0000

    generate guid for volume name

--- .teamcity/scripts/run-tests.step.sh
+++ .teamcity/scripts/run-tests.step.sh
@@ -4,23 +4,21 @@ function dc() {
 }
 
 cd "$(dirname "$0")/../../docker" || exit 4
+volume=$(< /proc/sys/kernel/random/uuid)
 
-dc build && \
-  docker volume rm vr_volume || true && \
-  dc up --abort-on-container-exit vr || true && \
-  dc run --rm --detach --name=post_vr --volume=vr_volume --entrypoint "sleep infinity" vr && \
-  docker cp post_vr:/src/visual_regressions/ci_report    ../web/visual_regressions && \
-  docker cp post_vr:/src/visual_regressions/html_report  ../web/visual_regressions && \
-  docker cp post_vr:/src/visual_regressions/bitmaps_test ../web/visual_regressions && \
-  docker cp post_vr:/src/visual_regressions/pdf_test ../web/visual_regressions && \
-  docker cp post_vr:/src/bin ../web && \
-  docker volume rm vr_volume || true && \
-  dc down
-result=$?
+dc build
+docker volume rm "$volume" || true
+dc up --abort-on-container-exit vr || true
+dc run --rm --detach --name=post_vr --volume="$volume" --entrypoint "sleep infinity" vr
+docker cp post_vr:/src/visual_regressions/ci_report    ../web/visual_regressions
+docker cp post_vr:/src/visual_regressions/html_report  ../web/visual_regressions
+docker cp post_vr:/src/visual_regressions/bitmaps_test ../web/visual_regressions
+docker cp post_vr:/src/visual_regressions/pdf_test ../web/visual_regressions
+docker cp post_vr:/src/bin ../web
+docker volume rm "$volume" || true
+dc down
 
 if [ ! -r "../web/visual_regressions/ci_report/xunit.xml" ]; then
   echo "No test report could be found after running the tests."
   exit 33
 fi
-
-exit $result
