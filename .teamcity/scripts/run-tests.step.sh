73d55f58fa1f011f23c4cb1278c7d5d331072a34
spen482 <spen482@users.noreply.github.com>
2023-12-25 11:37:15 +0000

    mm

--- .teamcity/scripts/run-tests.step.sh
+++ .teamcity/scripts/run-tests.step.sh
@@ -3,9 +3,15 @@ source "$(dirname "$0")/common.step.sh"
 
 export DEBUG=express:router
 export SERVER_PORT=15000
+composeFile="compose.test.yml"
 
-pnpm run test
+find .
+#pnpm run test
+docker-compose -f $composeFile build && \
+  docker-compose -f $composeFile up --abort-on-container-exit vr
 result=$?
+
+docker-compose -f $composeFile down
 
 if [ ! -r "web/visual_regressions/ci_report/xunit.xml" ]; then
   echo "No test report could be found after running the tests."
