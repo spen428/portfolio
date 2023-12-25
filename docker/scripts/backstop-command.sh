f26a6127c4634c5bd9890e2905b7d1c9e7f8449b
spen482 <spen482@users.noreply.github.com>
2023-12-25 01:58:13 +0000

    o

--- docker/scripts/backstop-command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y ghostscript
-
-npm install -g pnpm
-pnpm install
-
-node scripts/export-pdf.js --url="$BASE_URL" && \
-  . scripts/generate-pngs-from-pdf.sh && \
-  backstop test --configPath=backstop.cjs
