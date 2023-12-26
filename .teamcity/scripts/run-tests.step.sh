c8a21220aa2bd9e98639aca35ca309fd7f8de72c
spen482 <spen482@users.noreply.github.com>
2023-12-26 02:27:25 +0000

    Hopefully improve the script

--- .teamcity/scripts/run-tests.step.sh
+++ .teamcity/scripts/run-tests.step.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-projectName=$(< /proc/sys/kernel/random/uuid)
-
-function dc() {
-  docker-compose --project-name $projectName -f compose.test.yml "$@"
+dc() {
+  docker-compose --project-name "$PROJECT_NAME" -f compose.test.yml "$@"
 }
 
-cd "$(dirname "$0")/../../docker" || exit 4
-export VR_VOLUME_NAME=$(< /proc/sys/kernel/random/uuid)
+start_stack() {
+  dc build || exit 5
+  docker volume rm "$VR_VOLUME_NAME" || true
+  dc up --abort-on-container-exit vr || exit 6
+}
 
-dc build
-docker volume rm "$VR_VOLUME_NAME" || true
-dc up --abort-on-container-exit vr
-container=$(dc run --rm --detach --volume="$VR_VOLUME_NAME" --entrypoint "sleep infinity" vr)
-docker cp $container:/src/visual_regressions/ci_report    ../web/visual_regressions
-docker cp $container:/src/visual_regressions/html_report  ../web/visual_regressions
-docker cp $container:/src/visual_regressions/bitmaps_test ../web/visual_regressions
-docker cp $container:/src/visual_regressions/pdf_test ../web/visual_regressions
-docker cp $container:/src/bin ../web
-dc down
-docker volume rm "$VR_VOLUME_NAME" || true
+stop_stack() {
+  dc down || exit 9
+  docker volume rm "$VR_VOLUME_NAME" || true
+}
+
+copy_results_to_host() {
+  container=$(dc run --rm --detach --volume="$VR_VOLUME_NAME" --entrypoint "sleep infinity" vr || exit 7)
+  docker cp $container:/src/visual_regressions/ci_report ../web/visual_regressions && \
+    docker cp $container:/src/visual_regressions/html_report ../web/visual_regressions && \
+    docker cp $container:/src/visual_regressions/bitmaps_test ../web/visual_regressions && \
+    docker cp $container:/src/visual_regressions/pdf_test ../web/visual_regressions && \
+    docker cp $container:/src/bin ../web || exit 8
+}
+
+# shellcheck disable=SC2155
+export PROJECT_NAME="$(< /proc/sys/kernel/random/uuid)"
+# shellcheck disable=SC2155
+export VR_VOLUME_NAME="$(< /proc/sys/kernel/random/uuid)"
+export DATA_PATH="${DATA_PATH:-dummy}"
+
+cd "$(dirname "$0")/../../docker" || exit 4
+
+start_stack
+copy_results_to_host
+stop_stack
 
 if [ ! -r "../web/visual_regressions/ci_report/xunit.xml" ]; then
   echo "No test report could be found after running the tests."
