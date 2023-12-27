142612ed831e6285123218384d71e7a602b66618
spen482 <spen482@users.noreply.github.com>
2023-12-27 13:53:36 +0000

    Copy new master references to a separate folder

--- .teamcity/scripts/run-tests-against-master.step.sh
+++ .teamcity/scripts/run-tests-against-master.step.sh
@@ -3,3 +3,8 @@ source "$(dirname "$0")/common.sh"
 export DATA_PATH="data"
 export IMAGE_TAG="$COMMIT_HASH"
 source "$(dirname "$0")/run-tests.step.sh"
+
+cd "$(dirname "$0")../.." || exit 76
+mkdir new_reference
+cp web/visual_regressions/bitmaps_test/*/* new_reference/
+rm new_reference/report.json new_reference/failed_diff*
