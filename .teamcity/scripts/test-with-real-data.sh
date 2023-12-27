ea5e9d26e87041089e2e93f4d73398df18cd418d
spen482 <spen482@users.noreply.github.com>
2023-12-27 22:32:24 +0000

    Missed a slash

--- .teamcity/scripts/test-with-real-data.sh
+++ .teamcity/scripts/test-with-real-data.sh
@@ -4,7 +4,7 @@ export DATA_PATH="data"
 export IMAGE_TAG="$COMMIT_HASH"
 source "$(dirname "$0")/run-tests.step.sh"
 
-cd "$(dirname "$0")../.." || exit 76
+cd "$(dirname "$0")/../.." || exit 76
 mkdir new_reference
 cp web/visual_regressions/bitmaps_test/*/* new_reference/
 rm new_reference/report.json new_reference/failed_diff*
