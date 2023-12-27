90797fde36634146305b0dc2511ba53b6b3bd7f5
Samuel P <77985620+spen428@users.noreply.github.com>
2023-12-27 22:01:10 +0000

    Fixes for deploy script (#9)

--- .teamcity/scripts/deploy.sh
+++ .teamcity/scripts/deploy.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 PORTAINER_STACK_NAME="portfolio"
 DATA_PATH="${DATA_PATH:-"data"}"
-IMAGE_TAG="$(< image-tag.txt)"
+IMAGE_TAG="${IMAGE_TAG:-$(< image-tag.txt)}"
 
 if [ -z "$PORTAINER_ACCESS_TOKEN" ]; then
   echo "You must export the PORTAINER_ACCESS_TOKEN environment variable."
+  exit 1
+fi
+
+if [ -z "$CORS_ORIGINS" ]; then
+  echo "You must export the CORS_ORIGINS environment variable."
+  exit 1
+fi
+
+if [ -z "$VITE_API_URL" ]; then
+  echo "You must export the VITE_API_URL environment variable."
+  exit 1
+fi
+
+if [ -z "$IMAGE_TAG" ]; then
+  echo "You must export the IMAGE_TAG environment variable."
   exit 1
 fi
 
@@ -22,17 +37,37 @@ if [ "$stacks" != "[]" ]; then
   echo "Deleting stack..."
   stackId=$(node -e "console.log(JSON.parse('$stacks')[0].Id);")
   endpointId=$(node -e "console.log(JSON.parse('$stacks')[0].EndpointId);")
-  curl --fail-with-body -X DELETE "https://portainer.lykat.co.uk/api/stacks/$stackId?endpointId=$endpointId" \
-     -H "X-API-Key: $PORTAINER_ACCESS_TOKEN" || exit 3
+  status_code=$(curl -w '%{http_code}' -o body.txt -X DELETE "https://portainer.lykat.co.uk/api/stacks/$stackId?endpointId=$endpointId" \
+     -H "X-API-Key: $PORTAINER_ACCESS_TOKEN")
+  cat body.txt && rm body.txt
+  if [ $status_code -ge 400 ]; then
+    echo "curl returned HTTP $status_code"
+    exit 3
+  fi
   echo "Successfully deleted existing stack $stackId"
 fi
 
 echo "Creating stack '$PORTAINER_STACK_NAME' on endpoint $PORTAINER_ENDPOINT_ID..."
-dockerComposeFilePath="$(dirname "$0")/../../docker/compose.host.yml"
-curl --fail-with-body -X POST "https://portainer.lykat.co.uk/api/stacks/create/standalone/file?endpointId=$PORTAINER_ENDPOINT_ID" \
+
+sed 's#$DATA_PATH#'"${DATA_PATH}#" "$(dirname "$0")/../../docker/compose.host.yml" \
+  | sed 's#${IMAGE_TAG}#'"${IMAGE_TAG}#" \
+  | sed 's#$VITE_API_URL#'"${VITE_API_URL}#" \
+  | sed 's#$CORS_ORIGINS#'"${CORS_ORIGINS}#" > compose.yml
+
+echo "Deploying the following docker-compose body:"
+cat compose.yml
+
+status_code=$(curl -w '%{http_code}' -o body.txt -X POST "https://portainer.lykat.co.uk/api/stacks/create/standalone/file?endpointId=$PORTAINER_ENDPOINT_ID" \
    -H "X-API-Key: $PORTAINER_ACCESS_TOKEN" \
    -F "Name=$PORTAINER_STACK_NAME" \
    -F "Env=[{\"name\":\"DATA_PATH\",\"value\":\"$DATA_PATH\"},{\"name\":\"IMAGE_TAG\",\"value\":\"$IMAGE_TAG\"}]" \
-   -F "file=@$dockerComposeFilePath" || exit 4
+   -F "file=@compose.yml")
+
+rm compose.yml
+cat body.txt && rm body.txt
+if [ $status_code -ge 400 ]; then
+  echo "curl returned HTTP $status_code"
+  exit 4
+fi
 
 echo "Deployment complete"
