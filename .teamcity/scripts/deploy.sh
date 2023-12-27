e86ec8817b9a4b5df0f6d676e38d842d41e29299
spen482 <spen482@users.noreply.github.com>
2023-12-27 22:37:12 +0000

    Extract Portainer URL to env var

--- .teamcity/scripts/deploy.sh
+++ .teamcity/scripts/deploy.sh
@@ -23,21 +23,26 @@ if [ -z "$IMAGE_TAG" ]; then
   exit 16
 fi
 
+if [ -z "$PORTAINER_URL" ]; then
+  echo "You must export the PORTAINER_URL environment variable."
+  exit 15
+fi
+
 echo "Getting endpoints..."
-endpoints="$(curl -X GET "https://portainer.lykat.co.uk/api/endpoints?excludeSnapshots=true" -H "X-API-Key: $PORTAINER_ACCESS_TOKEN")"
+endpoints="$(curl -X GET "$PORTAINER_URL/api/endpoints?excludeSnapshots=true" -H "X-API-Key: $PORTAINER_ACCESS_TOKEN")"
 echo "$endpoints"
 PORTAINER_ENDPOINT_ID=$(node -e "console.log(JSON.parse('$endpoints')[0].Id);")
 echo "We will deploy to endpointId $PORTAINER_ENDPOINT_ID"
 
 echo "Getting stacks..."
-stacks=$(curl -X GET "https://portainer.lykat.co.uk/api/stacks" -H "X-API-Key: $PORTAINER_ACCESS_TOKEN")
+stacks=$(curl -X GET "$PORTAINER_URL/api/stacks" -H "X-API-Key: $PORTAINER_ACCESS_TOKEN")
 echo "$stacks"
 
 if [ "$stacks" != "[]" ]; then
   echo "Deleting stack..."
   stackId=$(node -e "console.log(JSON.parse('$stacks')[0].Id);")
   endpointId=$(node -e "console.log(JSON.parse('$stacks')[0].EndpointId);")
-  status_code=$(curl -w '%{http_code}' -o body.txt -X DELETE "https://portainer.lykat.co.uk/api/stacks/$stackId?endpointId=$endpointId" \
+  status_code=$(curl -w '%{http_code}' -o body.txt -X DELETE "$PORTAINER_URL/api/stacks/$stackId?endpointId=$endpointId" \
      -H "X-API-Key: $PORTAINER_ACCESS_TOKEN")
   cat body.txt && rm body.txt
   if [ $status_code -ge 400 ]; then
@@ -57,7 +62,7 @@ sed 's#$DATA_PATH#'"${DATA_PATH}#" "$(dirname "$0")/../../docker/compose.host.ym
 echo "Deploying the following docker-compose body:"
 cat compose.yml
 
-status_code=$(curl -w '%{http_code}' -o body.txt -X POST "https://portainer.lykat.co.uk/api/stacks/create/standalone/file?endpointId=$PORTAINER_ENDPOINT_ID" \
+status_code=$(curl -w '%{http_code}' -o body.txt -X POST "$PORTAINER_URL/api/stacks/create/standalone/file?endpointId=$PORTAINER_ENDPOINT_ID" \
    -H "X-API-Key: $PORTAINER_ACCESS_TOKEN" \
    -F "Name=$PORTAINER_STACK_NAME" \
    -F "Env=[{\"name\":\"DATA_PATH\",\"value\":\"$DATA_PATH\"},{\"name\":\"IMAGE_TAG\",\"value\":\"$IMAGE_TAG\"}]" \
