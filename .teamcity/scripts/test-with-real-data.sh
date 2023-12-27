af9caaab5fc53cc973cfd462b9f30edd9513b981
spen482 <spen482@users.noreply.github.com>
2023-12-27 23:19:06 +0000

    Another change lost to an evil rebase

--- .teamcity/scripts/test-with-real-data.sh
+++ .teamcity/scripts/test-with-real-data.sh
@@ -5,6 +5,7 @@ export IMAGE_TAG="$COMMIT_HASH"
 source "$(dirname "$0")/run-tests.step.sh"
 
 cd "$(dirname "$0")/../.." || exit 76
+echo "$IMAGE_TAG" > "image-tag.txt"
 mkdir new_reference
 cp web/visual_regressions/bitmaps_test/*/* new_reference/
 rm new_reference/report.json new_reference/failed_diff*
