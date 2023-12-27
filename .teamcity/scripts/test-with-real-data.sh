85e6299501e6669516cfff4cf478902be3805e73
spen482 <spen482@users.noreply.github.com>
2023-12-27 20:14:14 +0000

    remove file

--- .teamcity/scripts/test-with-real-data.sh
+++ .teamcity/scripts/test-with-real-data.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-source "$(dirname "$0")/run-tests-against-master.sh"
+source "$(dirname "$0")/common.sh"
+export DATA_PATH="data"
+export IMAGE_TAG="$COMMIT_HASH"
+source "$(dirname "$0")/run-tests.step.sh"
+
+cd "$(dirname "$0")../.." || exit 76
+mkdir new_reference
+cp web/visual_regressions/bitmaps_test/*/* new_reference/
+rm new_reference/report.json new_reference/failed_diff*
