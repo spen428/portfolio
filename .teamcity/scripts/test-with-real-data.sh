4b68fde48d74653539814977bc12986bb4d93bcb
Samuel P <77985620+spen428@users.noreply.github.com>
2023-12-28 01:46:06 +0000

    Deploy fixes (#10)

--- .teamcity/scripts/test-with-real-data.sh
+++ .teamcity/scripts/test-with-real-data.sh
@@ -2,9 +2,12 @@
 source "$(dirname "$0")/common.sh"
 export DATA_PATH="data"
 export IMAGE_TAG="$COMMIT_HASH"
+
 source "$(dirname "$0")/run-tests.step.sh"
 
-cd "$(dirname "$0")../.." || exit 76
+cd "$(dirname "$0")/../.." || exit 76
+pwd
+echo "$IMAGE_TAG" > "image-tag.txt"
 mkdir new_reference
 cp web/visual_regressions/bitmaps_test/*/* new_reference/
 rm new_reference/report.json new_reference/failed_diff*
