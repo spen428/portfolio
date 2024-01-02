057413407a2e3b382612b652ac0d4928ad74f6e5
spen482 <spen482@users.noreply.github.com>
2024-01-02 19:12:51 +0000

    Delete the bac

--- web/src/pages/business-card/BusinessCardPage.vue
+++ web/src/pages/business-card/BusinessCardPage.vue
@@ -4,23 +4,16 @@
       :style="{ height: '55mm', width: cardWidth }"
       :personal-info="personalInfo"
     />
-    <BusinessCardBack
-      v-show="false"
-      :style="{ height: '55mm', width: cardWidth }"
-      :cv-data="cvData"
-    />
   </div>
 </template>
 
 <script setup lang="ts">
 import BusinessCardFront from "@/pages/business-card/components/BusinessCardFront.vue";
-import BusinessCardBack from "@/pages/business-card/components/BusinessCardBack.vue";
 import DataService from "@/services/data.service";
 import LocaleService from "@/services/locale.service";
 import { computed } from "vue";
 
 const personalInfo = DataService.getPersonalInfo();
-const cvData = DataService.getCvData();
 const cardWidth = computed(() => {
   const locale = LocaleService.getCurrentLocale();
   return locale.value === "ja-JP" ? "99mm" : "85mm";
