6abcf109ebe705fee7ad3fe167498debb3b8daf1
spen482 <spen482@users.noreply.github.com>
2024-01-05 02:26:30 +0000

    custom video markdown proof of concept

--- web/src/components/MarkdownRenderer.vue
+++ web/src/components/MarkdownRenderer.vue
@@ -1,32 +1,82 @@
 <template>
-  <div v-html="renderedMarkdown" />
+  <template v-for="item in contentItems">
+    <div v-if="item.type === 'html'" v-html="item.content" />
+    <MediaWithLoadingSkeleton
+      v-if="item.type === 'video'"
+      :src="item.attrs![0][1]"
+      autoplay
+    />
+  </template>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import MarkdownIt from "markdown-it";
-import type { RenderRule } from "markdown-it/lib/renderer";
+import { computed, type ComputedRef } from "vue";
+import MarkdownIt, { type Token } from "markdown-it";
+import MediaWithLoadingSkeleton from "@/components/MediaWithLoadingSkeleton.vue";
+import type StateCore from "markdown-it/lib/rules_core/state_core";
 
-const markdownInstance = new MarkdownIt({
+const opts = {
   html: true,
   xhtmlOut: false,
   breaks: false,
   linkify: false,
+};
+const markdownInstance = new MarkdownIt(opts);
+
+markdownInstance.core.ruler.push("custom_video", (state: StateCore) => {
+  for (let i = 0; i < state.tokens.length; i++) {
+    const t = state.tokens[i];
+    if (!t.content.startsWith("#! ")) {
+      continue;
+    }
+    let token = new state.Token("video", "video", 0);
+    let src = t.content.split("#! ", 2)[1];
+    token.attrSet("src", src);
+    state.tokens[i] = token;
+  }
 });
 
-const originalRenderFn = markdownInstance.renderer.rules.html_block!;
-const customRenderFn: RenderRule = (tokens, idx, options, env, self) => {
-  const token = tokens[idx];
-  if (token.type === "html_block") {
-    if (token.content.startsWith("<video")) {
+const props = defineProps<{ source: string }>();
+
+type PartialToken = Pick<Token, "type" | "content" | "attrs">;
+
+const contentItems: ComputedRef<PartialToken[]> = computed(() => {
+  const tokens = markdownInstance.parse(props.source, {});
+  const items = [];
+  let currentToken, content;
+  for (let i = 0; i < tokens.length; i++) {
+    currentToken = tokens[i];
+
+    if (currentToken.type.endsWith("_open")) {
+      if (
+        i + 2 < tokens.length &&
+        tokens[i + 1].type === "video" &&
+        tokens[i + 2].type === "paragraph_close"
+      ) {
+        items.push(tokens[i + 1]);
+        i += 2;
+        continue;
+      }
+
+      content = "";
+      i++;
+      while (i < tokens.length && !tokens[i].type.endsWith("_close")) {
+        content += markdownInstance.renderer.render(
+          [tokens[i]],
+          markdownInstance.options,
+          {}
+        );
+        i++;
+      }
+
+      items.push({
+        type: "html",
+        content: `<${currentToken.tag}>${content}</${currentToken.tag}>`,
+        attrs: [],
+      });
     }
   }
 
-  return originalRenderFn(tokens, idx, options, env, self);
-};
-markdownInstance.renderer.rules.html_block = customRenderFn;
-
-const renderedMarkdown = computed(() => markdownInstance.render(props.source));
-
-const props = defineProps<{ source: string }>();
+  return items;
+});
 </script>
